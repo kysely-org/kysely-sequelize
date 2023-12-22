6a82581e19d9f5d2a0a5a7c1bc207b005329d086
igalklebanov <igalklebanov@gmail.com>
2023-12-23 01:01:39 +0200

    test

--- tests/nodejs/index.test.ts
+++ tests/nodejs/index.test.ts
@@ -1,10 +1,20 @@
 import {expect} from 'chai'
+import {DeleteResult, InferResult, InsertResult, Selectable, UpdateResult, sql} from 'kysely'
 import {jsonArrayFrom as jsonArrayFromMySQL} from 'kysely/helpers/mysql'
 import {jsonArrayFrom as jsonArrayFromPostgres} from 'kysely/helpers/postgres'
-import {SUPPORTED_DIALECTS} from '../../src/supported-dialects.js'
+import {jsonArrayFrom as jsonArrayFromSQLite} from 'kysely/helpers/sqlite'
+import {SUPPORTED_DIALECTS, SupportedDialect} from '../../src/supported-dialects.js'
 import {PersonModel} from './models/person.model.js'
 import {PetModel} from './models/pet.model.js'
-import {DEFAULT_DATA_SET, TestContext, initTest, seedDatabase} from './test-setup.js'
+import {
+  DEFAULT_DATA_SET,
+  Pet,
+  dropDatabase,
+  initTest,
+  seedDatabase,
+  truncateDatabase,
+  type TestContext,
+} from './test-setup.js'
 
 for (const dialect of SUPPORTED_DIALECTS) {
   describe(`KyselySequelizeDialect: ${dialect}`, () => {
@@ -13,7 +23,8 @@ for (const dialect of SUPPORTED_DIALECTS) {
     const jsonArrayFrom = {
       mysql: jsonArrayFromMySQL,
       postgres: jsonArrayFromPostgres,
-    }[dialect]
+      sqlite: jsonArrayFromSQLite,
+    }[dialect] as typeof jsonArrayFromMySQL
 
     before(async function () {
       ctx = await initTest(this, dialect)
@@ -24,11 +35,11 @@ for (const dialect of SUPPORTED_DIALECTS) {
     })
 
     afterEach(async () => {
-      await ctx.sequelize.truncate({cascade: true})
+      await truncateDatabase(ctx)
     })
 
     after(async () => {
-      await ctx.sequelize.drop()
+      await dropDatabase(ctx)
       await ctx.kysely.destroy()
     })
 
@@ -40,32 +51,127 @@ for (const dialect of SUPPORTED_DIALECTS) {
 
       expect(ormPeople).to.deep.equal(DEFAULT_DATA_SET)
 
-      const queryBuilderPeople = await ctx.kysely
+      const baseQuery = ctx.kysely
         .selectFrom('person')
-        .select((eb) => [
-          'firstName',
-          'gender',
-          'lastName',
-          'maritalStatus',
-          'middleName',
-          jsonArrayFrom(
-            eb.selectFrom('pet').whereRef('pet.ownerId', '=', 'person.id').select(['pet.name', 'pet.species']),
-          ).as('pets'),
-        ])
-        .execute()
+        .select(['firstName', 'gender', 'lastName', 'maritalStatus', 'middleName'])
+
+      const query = {
+        [dialect]: jsonArrayFrom
+          ? baseQuery.select((eb) =>
+              jsonArrayFrom(
+                eb.selectFrom('pet').whereRef('pet.ownerId', '=', 'person.id').select(['pet.name', 'pet.species']),
+              ).as('pets'),
+            )
+          : baseQuery,
+        mssql: baseQuery
+          .leftJoin('pet as pets', 'pets.ownerId', 'person.id')
+          .select(['pets.name', 'pets.species'])
+          .modifyEnd(sql`for json auto, include_null_values`)
+          .$castTo<InferResult<typeof baseQuery> & {pets: Selectable<Pick<Pet, 'name' | 'species'>>[]}>(),
+      }[dialect]
+
+      const queryBuilderPeople = await query.execute()
 
       expect(queryBuilderPeople).to.deep.equal(ormPeople)
     })
 
     it('should be able to perform insert queries', async () => {
-      const results = await ctx.kysely.insertInto('person').values({gender: 'female'}).execute()
+      const result = await ctx.kysely.insertInto('person').values({gender: 'female'}).executeTakeFirstOrThrow()
 
-      expect(results).to.deep.equal(
-        {
-          mysql: [{insertId: BigInt(DEFAULT_DATA_SET.length + 1), numInsertedOrUpdatedRows: BigInt(1)}],
-          postgres: [{insertId: undefined, numInsertedOrUpdatedRows: BigInt(1)}],
-        }[dialect],
+      expect(result).to.deep.equal(
+        (
+          {
+            mssql: {insertId: undefined, numInsertedOrUpdatedRows: BigInt(1)},
+            mysql: {insertId: BigInt(DEFAULT_DATA_SET.length + 1), numInsertedOrUpdatedRows: BigInt(1)},
+            postgres: {insertId: undefined, numInsertedOrUpdatedRows: BigInt(1)},
+            sqlite: {insertId: BigInt(DEFAULT_DATA_SET.length + 1), numInsertedOrUpdatedRows: BigInt(1)},
+          } satisfies Record<SupportedDialect, {[K in keyof InsertResult]: InsertResult[K]}>
+        )[dialect],
       )
     })
+
+    if (dialect === 'postgres' || dialect === 'sqlite') {
+      it('should be able to perform insert queries with returning', async () => {
+        const result = await ctx.kysely
+          .insertInto('person')
+          .values({gender: 'female'})
+          .returning('id')
+          .executeTakeFirst()
+
+        expect(result).to.deep.equal(
+          (
+            {
+              postgres: {id: DEFAULT_DATA_SET.length + 1},
+              // FIXME: sequelize/sqlite3 bug? its returning metadata, but no rows.
+              sqlite: undefined,
+            } satisfies Record<'postgres' | 'sqlite', unknown>
+          )[dialect],
+        )
+      })
+    }
+
+    it('should be able to perform update queries', async () => {
+      const result = await ctx.kysely
+        .updateTable('person')
+        .set({maritalStatus: 'widowed'})
+        .where('id', '=', 1)
+        .executeTakeFirstOrThrow()
+
+      expect(result).to.deep.equal(
+        (
+          {
+            mssql: {numChangedRows: undefined, numUpdatedRows: BigInt(1)},
+            mysql: {numChangedRows: BigInt(1), numUpdatedRows: BigInt(1)},
+            postgres: {numChangedRows: undefined, numUpdatedRows: BigInt(1)},
+            sqlite: {
+              numChangedRows: undefined,
+              // FIXME: sequelize/sqlite3 bug? its returning empty metadata and rows.
+              numUpdatedRows: BigInt(0),
+            },
+          } satisfies Record<SupportedDialect, {[K in keyof UpdateResult]: UpdateResult[K]}>
+        )[dialect],
+      )
+    })
+
+    if (dialect === 'postgres' || dialect === 'sqlite') {
+      it('should be able to perform update queries with returning', async () => {
+        const result = await ctx.kysely
+          .updateTable('person')
+          .set({maritalStatus: 'widowed'})
+          .where('id', '=', 1)
+          .returning(['gender'])
+          .executeTakeFirstOrThrow()
+
+        expect(result).to.deep.equal({gender: DEFAULT_DATA_SET[0].gender})
+      })
+    }
+
+    it('should be able to perform delete queries', async () => {
+      const result = await ctx.kysely.deleteFrom('person').where('id', '=', 1).executeTakeFirstOrThrow()
+
+      expect(result).to.deep.equal(
+        (
+          {
+            mssql: {numDeletedRows: BigInt(1)},
+            mysql: {numDeletedRows: BigInt(1)},
+            postgres: {numDeletedRows: BigInt(1)},
+            // FIXME: sequelize/sqlite3 bug? its returning empty metadata and rows.
+            sqlite: {numDeletedRows: BigInt(0)},
+          } satisfies Record<SupportedDialect, {[K in keyof DeleteResult]: DeleteResult[K]}>
+        )[dialect],
+      )
+    })
+
+    if (dialect === 'postgres' || dialect === 'sqlite') {
+      it('should be able to perform delete queries with returning', async () => {
+        const result = await ctx.kysely
+          .deleteFrom('person')
+          .where('id', '=', 1)
+          .returning('gender')
+          .executeTakeFirstOrThrow()
+
+        expect(result).to.deep.equal({gender: DEFAULT_DATA_SET[0].gender})
+      })
+    }
   })
 }
