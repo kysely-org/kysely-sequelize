91c789f75ab48c93525fd9e59e8abab225411077
igalklebanov <igalklebanov@gmail.com>
2023-12-23 00:58:14 +0200

    connection.

--- src/connection.ts
+++ src/connection.ts
@@ -1,7 +1,8 @@
 import {type CompiledQuery, type DatabaseConnection, type QueryResult, type TransactionSettings} from 'kysely'
-import {QueryTypes, type Sequelize, type Transaction, type TransactionOptions} from 'sequelize'
+import {QueryOptions, type Sequelize, type Transaction, type TransactionOptions} from 'sequelize'
 import {ISOLATION_LEVELS} from './isolation-levels.js'
-import {isNumber} from './type-utils.js'
+import {SupportedDialect} from './supported-dialects.js'
+import {isNumber, isObject, isString} from './type-utils.js'
 
 export class KyselySequelizeConnection implements DatabaseConnection {
   readonly #sequelize: Sequelize
@@ -46,22 +47,24 @@ export class KyselySequelizeConnection implements DatabaseConnection {
   }
 
   async executeQuery<R>(compiledQuery: CompiledQuery<unknown>): Promise<QueryResult<R>> {
+    const query = this.#getQuery(compiledQuery)
     const queryOptions = await this.#getQueryOptions(compiledQuery)
 
-    const [rows, metadata] = await this.#sequelize.query(compiledQuery.sql, queryOptions)
+    const [rows, metadata] = await this.#sequelize.query(query, queryOptions)
 
     return {
-      insertId: isNumber(rows) ? BigInt(rows) : undefined,
-      numAffectedRows: isNumber(metadata) ? BigInt(metadata) : undefined,
-      rows: isNumber(rows) ? [] : (rows as R[]),
+      ...this.#getLastInsertedIdAndNumAffectedRows(rows, metadata),
+      rows: Array.isArray(rows) ? (rows as R[]) : [],
     }
   }
 
   async *streamQuery<R>(
-    compiledQuery: CompiledQuery<unknown>,
-    chunkSize?: number | undefined,
+    _compiledQuery: CompiledQuery<unknown>,
+    _chunkSize?: number | undefined,
   ): AsyncIterableIterator<QueryResult<R>> {
-    throw new Error('Method not implemented.')
+    throw new Error(
+      'Sequelize does not support streaming queries yet! follow https://github.com/sequelize/sequelize/issues/15827',
+    )
   }
 
   #translateTransactionSettings(settings: TransactionSettings): TransactionOptions {
@@ -71,33 +74,91 @@ export class KyselySequelizeConnection implements DatabaseConnection {
     }
   }
 
-  async #getQueryOptions(compiledQuery: CompiledQuery<unknown>) {
-    const queryType = {
-      [compiledQuery.query.kind]: QueryTypes.RAW,
-      ['SelectQueryNode']: QueryTypes.SELECT,
-      ['InsertQueryNode']: QueryTypes.INSERT,
-      ['UpdateQueryNode']: QueryTypes.UPDATE,
-      ['DeleteQueryNode']: QueryTypes.DELETE,
-    }[compiledQuery.query.kind]
+  #getQuery(compiledQuery: CompiledQuery<unknown>): string {
+    const dialect = this.#sequelize.getDialect() as SupportedDialect
 
-    const paramsKey = this.#sequelize.getDialect() === 'mysql' ? 'replacements' : 'bind'
+    if (dialect === 'mssql') {
+      let {sql} = compiledQuery
 
-    if (this.#transaction) {
+      compiledQuery.parameters.forEach((parameter, index) => {
+        sql = sql.replace(
+          new RegExp(`@${index + 1}([^\\d])?`),
+          isString(parameter) ? `N'${parameter}'$1` : `${parameter}$1`,
+        )
+      })
+
+      return sql
+    }
+
+    return compiledQuery.sql
+  }
+
+  async #getQueryOptions(compiledQuery: CompiledQuery<unknown>): Promise<QueryOptions> {
+    const dialect = this.#sequelize.getDialect() as SupportedDialect
+    const paramsKey = ['mysql', 'sqlite'].includes(dialect)
+      ? 'replacements'
+      : ['postgres'].includes(dialect)
+      ? 'bind'
+      : undefined
+
+    return {
+      ...(paramsKey ? {[paramsKey]: compiledQuery.parameters} : {}),
+      transaction:
+        this.#transaction ||
+        ({
+          connection: (this.#connection ||= await this.#sequelize.connectionManager.getConnection({type: 'write'})),
+        } as unknown as Transaction),
+    }
+  }
+
+  #getLastInsertedIdAndNumAffectedRows(rows: unknown, metadata: unknown): Omit<QueryResult<any>, 'rows'> {
+    const dialect = this.#sequelize.getDialect() as SupportedDialect
+
+    if (dialect === 'postgres') {
       return {
-        [paramsKey]: [...compiledQuery.parameters],
-        queryType,
-        transaction: this.#transaction,
+        numAffectedRows: isNumber(metadata)
+          ? BigInt(metadata)
+          : isObject(metadata) && 'rowCount' in metadata && isNumber(metadata.rowCount)
+          ? BigInt(metadata.rowCount)
+          : undefined,
       }
     }
 
-    if (!this.#connection) {
-      this.#connection = await this.#sequelize.connectionManager.getConnection({type: 'write'})
+    if (dialect === 'mysql') {
+      return {
+        insertId: isNumber(rows) ? BigInt(rows) : undefined,
+        numAffectedRows: isNumber(metadata)
+          ? BigInt(metadata)
+          : isObject(metadata) && 'affectedRows' in metadata && isNumber(metadata.affectedRows)
+          ? BigInt(metadata.affectedRows)
+          : undefined,
+        numChangedRows:
+          isObject(metadata) && 'changedRows' in metadata && isNumber(metadata.changedRows)
+            ? BigInt(metadata.changedRows)
+            : undefined,
+      }
     }
 
-    return {
-      [paramsKey]: [...compiledQuery.parameters],
-      queryType,
-      transaction: {connection: this.#connection} as unknown as Transaction,
+    if (dialect === 'mssql') {
+      return {
+        insertId: isNumber(rows) ? BigInt(rows) : undefined,
+        numAffectedRows: isNumber(metadata) ? BigInt(metadata) : undefined,
+      }
     }
+
+    if (dialect === 'sqlite') {
+      if (!isObject(metadata)) {
+        return {}
+      }
+
+      const {changes, lastID} = metadata
+
+      return {
+        insertId: isNumber(lastID) ? BigInt(lastID) : undefined,
+        numAffectedRows: isNumber(changes) ? BigInt(changes) : undefined,
+      }
+    }
+
+    return {}
   }
 }
